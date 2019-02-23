07d82a9d69355ef4193b9dbaeb516e9f03231f6a
dharmendra chouhan <dharmendra.falna@gmail.com>
2019-02-23 21:52:26 +0530

    pytone commente / variables

---
+++ python-study/03_creating_variables.py
@@ -0,0 +1,20 @@
+x = 5 # x is of type of int
+y = "Dharmendra Chouhan Falna" # y is now of type string
+print(x)
+print(y)
+
+z =  "awesome"
+print("Python is "+z )
+
+x = "Pytone is "
+y = "awesome add a variable to other variable"
+z = x + y
+print(z)
+
+x = 5
+y = 10
+print(x+y)
+
+x = 5
+y = "Dharmendra chouhan"
+# print (x + y) # TypeError: unsupported operand type(s) for +: 'int' and 'str'
